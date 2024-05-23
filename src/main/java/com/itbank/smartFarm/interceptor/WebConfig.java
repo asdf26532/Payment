72fd9b85eb784b6939cebc9d3edef58cb915bd60
Komorebi <karan1284@naver.com>
2024-05-23 11:49:25 +0900

    Add Interceptor Setting up a login interceptor for chat-related pages

--- src/main/java/com/itbank/smartFarm/interceptor/WebConfig.java
+++ src/main/java/com/itbank/smartFarm/interceptor/WebConfig.java
@@ -15,6 +15,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/member/myPage", "/member/update");
+                .addPathPatterns("/member/myPage",
+                        "/member/update",
+                        "/chat/**");
     }
 }

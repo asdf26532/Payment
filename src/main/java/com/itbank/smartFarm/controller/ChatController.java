7476f1b93aa6de9eca24842f769de1853b82a990
Komorebi <karan1284@naver.com>
2024-05-23 12:07:11 +0900

    Repeating paths on a page

--- src/main/java/com/itbank/smartFarm/controller/ChatController.java
+++ src/main/java/com/itbank/smartFarm/controller/ChatController.java
@@ -7,9 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -20,27 +18,12 @@ public class ChatController {
     @Autowired
     private ChatService chatService;
 
-    @GetMapping("/chatScreen/{senderId}/{receiverId}")
-    public String chat(Model model, @PathVariable String senderId, @PathVariable String receiverId) {
-        model.addAttribute("sender_id", senderId);
-        model.addAttribute("receiver_id", receiverId);
-        return "/chat/chatScreen";
-    }
-
-    @GetMapping("/chatList")
-    public String chatList(Model model, HttpSession session) {
-        MemberVO user = (MemberVO) session.getAttribute("user");
-        List<MemberVO> senders = chatService.getSendersByReceiverId(user.getId());
-        model.addAttribute("senders", senders);
-        return "chat/chatList";
-    }
-
-    @GetMapping("/chatScreen")
+    @GetMapping()
     public String chatScreen(Model model, HttpSession session) {
         MemberVO user = (MemberVO) session.getAttribute("user");
         List<MemberVO> senders = chatService.getSendersByReceiverId(user.getId());
         model.addAttribute("senders", senders);
         model.addAttribute("sender_id", user.getId());
-        return "chat/chatScreen";
+        return "/chat/chat";
     }
 }

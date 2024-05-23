15f6bdefc20773d50b5e24bc943ac872a18dd67a
Komorebi <karan1284@naver.com>
2024-05-23 21:01:52 +0900

    Complete the chat system

--- src/main/java/com/itbank/smartFarm/model/ChatDAO.java
+++ src/main/java/com/itbank/smartFarm/model/ChatDAO.java
@@ -11,22 +11,26 @@ import java.util.List;
 @Mapper
 public interface ChatDAO {
 
-    @Select("SELECT m.*, u.nick, m.timestamp FROM message m JOIN member u ON m.sender_id = u.id " +
-            "WHERE (m.sender_id = #{senderId} AND m.receiver_id = #{receiverId}) " +
-            "OR (m.sender_id = #{receiverId} AND m.receiver_id = #{senderId}) " +
+    @Select("SELECT m.*, u.nick, m.timestamp, m.senderId, m.receiverId FROM message m JOIN member u ON m.senderId = u.id " +
+            "WHERE (m.senderId = #{senderId} AND m.receiverId = #{receiverId}) " +
+            "OR (m.senderId = #{receiverId} AND m.receiverId = #{senderId}) " +
             "ORDER BY m.timestamp")
     List<MessageVO> findBySenderIdAndReceiverId(int senderId, int receiverId);
 
-    @Insert("INSERT INTO message (sender_id, receiver_id, content, timestamp) VALUES (#{senderId}, #{receiverId}, #{content}, NOW())")
+    @Insert("INSERT INTO message (senderId, receiverId, content, timestamp) VALUES (#{senderId}, #{receiverId}, #{content}, NOW())")
     void insertMessage(MessageVO message);
 
-    @Select("SELECT m.*, u.nick, m.timestamp FROM message m JOIN member u ON m.sender_id = u.id WHERE m.id = #{messageId}")
+    @Select("SELECT m.*, u.nick, m.timestamp FROM message m JOIN member u ON m.senderId = u.id WHERE m.id = #{messageId}")
     MessageVO findMessageById(int messageId);
 
-    @Select("SELECT m.*, u.nick FROM message m JOIN member u ON m.sender_id = u.id WHERE m.id = LAST_INSERT_ID()")
+    @Select("SELECT m.*, u.nick FROM message m JOIN member u ON m.senderId = u.id WHERE m.id = LAST_INSERT_ID()")
     MessageVO findLastMessage();
 
-    @Select("SELECT DISTINCT u.* FROM message m JOIN member u ON m.sender_id = u.id WHERE m.receiver_id = #{receiverId}")
+    @Select("SELECT DISTINCT u.* " +
+            "FROM message m " +
+            "JOIN member u ON (m.senderId = u.id OR m.receiverId = u.id)" +
+            "WHERE (m.senderId = #{receiverId} OR m.receiverId = #{receiverId})" +
+            "AND u.id != #{receiverId}")
     List<MemberVO> findSendersByReceiverId(int receiverId);
 
 }

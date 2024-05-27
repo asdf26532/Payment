339ea4a95f6905f54896c1a8dd5205423a1c5476
SeungHwan Lee <asdf26532@gmail.com>
2024-05-27 21:38:15 +0900

    add comment

--- src/main/java/com/itbank/smartFarm/controller/DownloadController.java
+++ src/main/java/com/itbank/smartFarm/controller/DownloadController.java
@@ -24,7 +24,7 @@ public class DownloadController {
 
     @GetMapping
     public String downloadPage(Model model) {
-        // 다운로드할 파일 이름을 설정.
+        // 파일 이름 설정
         model.addAttribute("filename", "IoT.apk");
         return "download";
     }
@@ -33,14 +33,15 @@ public class DownloadController {
     @GetMapping("/{filename}")
     public ResponseEntity<Resource> downloadFile(@PathVariable String filename) {
         try {
-            // 파일을 로드합니다.
+            // 파일 로드
             Path file = ds.loadFile(filename);
             if (file == null) {
                 return ResponseEntity.notFound().build();
             }
-            // 파일을 리소스로 변환합니다.
+            // 파일을 리소스로 변환
             Resource resource = new UrlResource(file.toUri());
-            // 파일을 다운로드하도록 응답 헤더를 설정하고 파일을 응답 본문에 추가하여 반환합니다.
+
+            // 파일을 다운로드하도록 응답 헤더를 설정하고 파일을 바디에 추가 후 반환
             return ResponseEntity.ok()
                     .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
                     .body(resource);
--- src/main/java/com/itbank/smartFarm/service/DownloadService.java
+++ src/main/java/com/itbank/smartFarm/service/DownloadService.java
@@ -18,9 +18,9 @@ public class DownloadService {
     // 지정된 파일 이름을 사용하여 파일 경로를 로드하는 메서드
     public Path loadFile(String filename) {
         try {
-            // 파일 저장 경로를 설정
+            // 파일 저장 경로 설정
             Path file = Paths.get(downloadDir).resolve(filename);
-            // 파일이 존재 여부 확인
+            // 파일 존재 여부 확인
             if (Files.exists(file)) {
                 return file;
             } else {
